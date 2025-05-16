dac6d9d470719c5270313bbde2c4461d75a7e657
Gabriel Mendes <161656171+Mendes1801@users.noreply.github.com>
2025-05-16 20:53:51 -0300

    Add files via upload

---
+++ projeto (1).c
@@ -0,0 +1,19 @@
+struct processo
+{
+    char nome[20];
+    int pid;
+    int ppid;
+    int quantum;
+    int prioridade;
+    int tempo_execucao;
+    char[] texto;
+    char[] dados;
+    char *heap;
+    char *pilha;
+};
+
+main(){
+
+
+    return 0;
+}
